794a6fb173052d16db1f2d4e832b62e033370646
Don Schenck <don.schenck@gmail.com>
2017-01-11 14:50:19 -0500

    Initial code push.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace howdy 
+{
+    public class HomeController
+    {
+
+        [Route("/greeting")]
+        public String Get()
+        {
+            if (File.Exists("slowdown")) {
+                Thread.Sleep(9000);
+            }
+            return "Howdy";
+        }
+
+        [Route("/slowdown")]
+        public String Slowdown()
+        {
+            if (!(File.Exists("slowdown"))) {
+		File.Create("slowdown");
+            }
+            return "Slowdown initiated...";
+        }
+        
+        [Route("/normal")]
+        public String Normal()
+        {
+ 	    if (File.Exists("slowdown")) {
+                File.Delete("slowdown");
+            }
+            return "Back to normal speed...";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # howdy
 RESTful service that returns "howdy".
+
+This service has a mechanism built in to mimic an overloaded or slow server. This feature is controlled by the URI used to GET from this service:
+
+/greeting - this returns the string "Howdy". If the file 'slowdown' exists, the service will sleep for nine (9) seconds before returning, feigning a slow service.
+
+/slowdown - this creates a file ('slowdown'). Because a file is persistent across multiple sessions, subsequent calls to this service will see it and respond accordingly.
+
+/normal - this deletes the file 'slowdown'
+
+
+This normal/slowdown feature is highlighted by using the Circuit Breaker Pattern. A command-line client can be found at https://github.com/redhat-dotnet-msa/circuitbreaker_console_app.
